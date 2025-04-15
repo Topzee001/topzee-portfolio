5c36b85c853a9d0952301b86428caa24963bd8d7
Ibrahim <ibrahimsakariyah@gmail.com>
2025-04-15 06:56:12 +0100

    riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'pages/home.dart';
 
@@ -12,11 +13,13 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      title: 'Ibrahim',
-      home: const MyHomePage(),
+    return ProviderScope(
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark(),
+        title: 'Ibrahim',
+        home: const MyHomePage(),
+      ),
     );
   }
 }
